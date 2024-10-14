18781a15ae47056d08c838ad2c1b2193a540c03a
Hezekiah Munene <hezemunene01@gmail.com>
2024-10-14 11:40:36 +0300

    added tables

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -8,17 +8,21 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     * 
      */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id(); //must be present in all tables
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('user_type');
+            $table->string('phone');
+            $table->string('location');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); //creates three columns : created_at, updated_at, deleted_at
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+//VERY IMPORTANT
+
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
