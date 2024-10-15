fb1fcce3048c77ef48205977d3ec47803eb5a810
Hezekiah Munene <hezemunene01@gmail.com>
2024-10-15 10:20:15 +0300

    set user_id foreign key in orders

--- database/migrations/2024_10_14_083053_create_orders_table.php
+++ database/migrations/2024_10_14_083053_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
+            $table->foreignId('user_id')->constrained();
             $table->string('order_type');
             $table->string('order_status');
             $table->integer('order_total');
