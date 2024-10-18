52c272eb97bbf3852a0226acb39fae78ca3016af
Hezekiah Munene <hezemunene01@gmail.com>
2024-10-18 09:53:02 +0300

    added routes in api.php, added login fn in AuthC

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'user_type' => 'required |string',
+            'phone' => 'required|',
+            'location' => 'required|',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'user_type' => $request -> user_type,
+            'phone' => $request -> phone,
+            'location' => $request -> location,
+        ]);
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('token-name')->plainTextToken,
+        ], 201);
+    }
+
+    function login(Request $request){
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('token-name')->plainTextToken,
+        ]);
+    }
 }
